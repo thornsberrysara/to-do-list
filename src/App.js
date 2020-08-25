bb915c180c8b53eafda370608a6ab18de8f8a7ae
Sara Thornsberry <thornsberrysara@gmail.com>
2020-08-25 14:35:15 -0400

    Add functionality is working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import React from "react";
-import "./App.css";
+import './App.css';
+import ListItems from './ListItems';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        content: "",
+        key: ""
+      }
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        content: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if(newItem.content !== '') {
+      const newItems = [...this.state.items, newItem];
+      this.setState({
+        items: newItems,
+        currentItem: {
+          content: '',
+          key: ''
+        }
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
-      <header>
-        <form id="to-do-form">
-          <input type="text" placeholder="Add"/>
-          <button type="submit">Add</button>
-        </form>
-      </header>
+        <header>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input
+              type="text"
+              placeholder="Add"
+              value={this.state.currentItem.content}
+              onChange={this.handleInput}
+            />
+            <button type="submit">Add</button>
+          </form>
+          <ListItems items={this.state.items}/>
+        </header>
       </div>
     );
   }
---
+++ src/ListItems.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ListItems(props) {
+    const items = props.items;
+    const listItems = items.map(item =>
+        {
+            return <div className="list" key="item.key">
+            <p>{item.content}</p>
+            </div>
+        })
+    return (
+    <div>{listItems}</div>
+    );
+}
+
+export default ListItems;
