cf4c6bad6e3c4c98f413b4cf4492d5ae5f1381f3
Sara Thornsberry <thornsberrysara@gmail.com>
2020-08-25 14:09:45 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+      <header>
+        <form id="to-do-form">
+          <input type="text" placeholder="Add"/>
+          <button type="submit">Add</button>
+        </form>
       </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
