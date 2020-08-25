02d5e719d0c7f3d88fd3a16a540a8df03b2f246f
Sara Thornsberry <thornsberrysara@gmail.com>
2020-08-25 15:06:34 -0400

    Delete functionality is working

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ class App extends React.Component {
     };
     this.handleInput = this.handleInput.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+
   }
   handleInput(e) {
     this.setState({
@@ -37,6 +39,13 @@ class App extends React.Component {
       })
     }
   }
+  deleteItem(key) {
+    const filteredItems = this.state.items.filter(item => 
+      item.key!==key);
+      this.setState({
+        items:filteredItems
+      })
+  }
 
   render() {
     return (
@@ -45,13 +54,14 @@ class App extends React.Component {
           <form id="to-do-form" onSubmit={this.addItem}>
             <input
               type="text"
-              placeholder="Add"
+              placeholder="Wow, you're busy!"
               value={this.state.currentItem.content}
               onChange={this.handleInput}
             />
             <button type="submit">Add</button>
           </form>
-          <ListItems items={this.state.items}/>
+          <ListItems items={this.state.items}
+          deleteItem={this.deleteItem}/>
         </header>
       </div>
     );
--- src/ListItems.js
+++ src/ListItems.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React from "react";
 
 function ListItems(props) {
-    const items = props.items;
-    const listItems = items.map(item =>
-        {
-            return <div className="list" key="item.key">
-            <p>{item.content}</p>
-            </div>
-        })
+  const items = props.items;
+  const listItems = items.map(item => {
     return (
-    <div>{listItems}</div>
+      <div className="list" key={item.key}>
+        <p>
+          {item.content}
+          <span>
+            <i
+              className="fas fa-trash"
+              onClick={() => props.deleteItem(item.key)}
+            ></i>
+          </span>
+        </p>
+      </div>
     );
+  });
+  return <div>{listItems}</div>;
 }
 
 export default ListItems;
