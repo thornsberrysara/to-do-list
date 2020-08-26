7b0b8b1a7c96ae72ac6b5eabbfd2584bcc2a0563
Sara Thornsberry <thornsberrysara@gmail.com>
2020-08-26 09:17:13 -0400

    Updates

---
+++ precache-manifest.632c10837589896f6bb68f7844b575fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569f2c487652310bb9aea5ad32e6c83c",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "82de4a0ef3792fd91b15",
+    "url": "/to-do-list/static/css/main.23206704.chunk.css"
+  },
+  {
+    "revision": "23dd13b3bc4a5e127d8c",
+    "url": "/to-do-list/static/js/2.f860dc33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.f860dc33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82de4a0ef3792fd91b15",
+    "url": "/to-do-list/static/js/main.4359b16b.chunk.js"
+  },
+  {
+    "revision": "99620f4632993b275b31",
+    "url": "/to-do-list/static/js/runtime-main.65d94b1a.js"
+  }
+]);
